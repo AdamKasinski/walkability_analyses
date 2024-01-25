39f355acd0eb4a7995fcd309517b933f554206f6
Adam Kasinski <adam.kasinski9@gmail.com>
2024-01-25 02:13:23 +0100

    added additional function used to create maps/metadata

---
+++ prepare_data.jl
@@ -0,0 +1,43 @@
+using Downloads
+using OSMToolset
+using OpenStreetMapX
+using Downloads
+using GZip
+using DataFrames
+using CSV
+
+function download_data(city::String)
+    if isfile(string(city,".osm"))
+        return "The file is already downloaded"
+    else
+        url = string("https://download.bbbike.org/osm/bbbike/",city,"/",city,".osm.gz")
+        output = "file.osm.gz"
+        Downloads.download(url,output)
+        tname = string(city,".osm")
+        GZip.open("file.osm.gz", "r") do io
+            open(tname, "w") do out
+                write(out, read(io))
+            end
+        end
+        rm("file.osm.gz"; recursive=false)
+    end
+end
+
+function create_map(file::String)
+    return get_map_data(file,use_cache = false)
+end
+
+function save_as(file::String,save_as::String)
+    if save_as != ""
+        CSV.write("save_as",file)
+    end
+end
+
+function get_POI(file::String, save_as::String = "")
+    if file[end-2:end] == "osm"
+        save_file(file,save_as)
+        return OSMToolset.find_poi(file)
+    elseif file[end-2:end] == "csv"
+        return DataFrame(CSV.File(file))
+    end
+end
