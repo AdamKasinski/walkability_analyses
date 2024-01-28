50cefbc6a4d19e2cac661862029d24174ca427f6
Adam Kasinski <adam.kasinski9@gmail.com>
2024-01-28 01:31:46 +0100

    generate sector, calculate attractiveness function added

--- analyse.jl
+++ analyse.jl
@@ -1,41 +1,64 @@
 using OpenStreetMapX
 using OSMToolset
+using Statistics
+
+#TODO: 1) change find_point_within_distance to find_point_within_sector - add paraemeter bounds
+#      2) create function generate circles in city
+#      3) generate attractiveness of each point - change default configuration
+#      4) generate plots
+
+
 
 """
-generate n points around the center within a distance
+generate n points around the center within boundries 
 
-- 'distance'::Int - radius of circle in meters
+- 'boundries'::Int - segment on which a point will be drawn
 - 'centre'::LLA - centre of the circle
 - 'num_of_points'::Int - number of points to generate
-- 'within'::Bool - "yes" - points generated on the circumference  
-                    "no" - point generated inside the circle,
-                           the nearest point at a distance of 100 meters 
 """
-function generate_circle_of_points(distance::Int,centre::LLA,num_of_points::Int,within::Bool)
+
+function generate_points_in_sector(boundries::Tuple{Int,Int},centre::LLA,num_of_points::Int)
     radian::Float64 = 360/num_of_points*π/180
-    if within
-        points = [find_point_in_distance(distance, centre, point * radian) for 
-                                                    point in 1:num_of_points]
-    else 
-        points = [find_point_in_distance(distance, centre, point * radian) for 
-                                                    point in 1:num_of_points]
-    end
+    points = [find_point_within_boundries(boundries, centre, point * radian) for point in 1:num_of_points]
     return points
 end
 
 """
-generate a point at a specified distance from the center
-"""
-function find_point_in_distance(distance::Int, centre::LLA, radian::Float64)
-    return LLA(ENU(distance*cos(radian),distance*sin(radian),0),centre)
-end
+generate n sectors
 
+- 'num_of_sectors'::Int - numer of sector to generate
+- 'size_of_sector'::Int - radius of each sector
+- 'centre'::LLA - centre of the map
+- 'num_of_points'::Int - number of points to generate in each sector
+"""
+
+function generate_sectors(num_of_sectors::Int,size_of_sector::Int,centre::LLA,num_of_points::Int)
+    sectors = Array{Any,2}(undef,num_of_sectors,num_of_points)
+    for sector in 1:num_of_sectors
+        boundries::Tuple{Int,Int} = ((sector-1)*size_of_sector+1,sector*size_of_sector)
+        sectors[sector,:] = generate_points_in_sector(boundries,centre,num_of_points)
+    end
+    return sectors
+end 
 
 """
 generate a point on a segment of a specified length 
 """
-
-function find_point_in_distance(radius::Int, centre::LLA, radian::Float64)
-    distance = rand(100:radius)
+function find_point_within_boundries(boundries::Tuple{Int,Int}, centre::LLA, radian::Float64)
+    distance = rand(boundries[1]:boundries[2])
     return LLA(ENU(distance*cos(radian),distance*sin(radian),0),centre)
+end
+
+function calculate_attractiveness_of_sector(points_matrix,attractivenessSpatIndex)
+    dim1 = size(points_matrix,1)
+    dim2 = size(points_matrix,2)
+    attract = zeros(Float64,dim1)
+    for i in dim1
+        attr = zeros(Float64,dim2)
+        for j in dim2
+            attr[j] = OSMToolset.attractiveness(attractivenessSpatIndex,points[i,j])
+        end
+        attract[i] = mean(attr)
+    end
+    return attract
 end
