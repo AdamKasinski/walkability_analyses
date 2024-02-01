78d80976ef328356746fc802bb884b5aa096e54c
Adam Kasinski <adam.kasinski9@gmail.com>
2024-02-01 02:26:39 +0100

    points generated at a distance from center

--- analyse.jl
+++ analyse.jl
@@ -3,43 +3,47 @@ using OSMToolset
 using Statistics
 include("prepare_data.jl")
 
-"""
-generate n points around the center within boundries
 
-- 'boundries'::Int - segment on which a point will be drawn
-- 'centre'::LLA - centre of the circle
-- 'num_of_points'::Int - number of points to generate
-"""
-
-function generate_points_in_sector(boundries::Tuple{Int,Int},centre::LLA,num_of_points::Int)
-    radian::Float64 = 360/num_of_points*π/180
-    points = [find_point_within_boundries(boundries, centre, point * radian) for point in 1:num_of_points]
-    return points
-end
 
 """
 generate n sectors
 
-- 'num_of_sectors'::Int - numer of sector to generate
+- 'distance'::Int - numer of sector to generate
 - 'size_of_sector'::Int - radius of each sector
 - 'centre'::LLA - centre of the map
 - 'num_of_points'::Int - number of points to generate in each sector
 """
-function generate_sectors(num_of_sectors::Int,size_of_sector::Int,centre::LLA,num_of_points::Int)
+function generate_sectors(num_of_sectors::Int,distance::Int,centre::LLA,num_of_points::Int)
+    
     sectors = Array{LLA,2}(undef,num_of_sectors,num_of_points)
     for sector in 1:num_of_sectors
-        boundries::Tuple{Int,Int} = ((sector-1)*size_of_sector+1,sector*size_of_sector)
-        sectors[sector,:] = generate_points_in_sector(boundries,centre,num_of_points)
+        sectors[sector,:] = generate_points_in_sector(distance*sector,centre,num_of_points)
     end
     return sectors
 end
 
 """
-generate a point on a segment of a specified length
+generate n points around the center at a distance
+
+- 'distance'::Int - distance at which points will be generated
+- 'centre'::LLA - centre of the circle
+- 'num_of_points'::Int - number of points to generate
 """
-function find_point_within_boundries(boundries::Tuple{Int,Int}, centre::LLA, radian::Float64)
-    distance = rand(boundries[1]:boundries[2])
-    return LLA(ENU(distance*cos(radian),distance*sin(radian),0),centre)
+
+function generate_points_in_sector(distance::Int,centre::LLA,num_of_points::Int)
+    radian::Float64 = 360/num_of_points*π/180
+    points = [find_point_at_distance(distance, centre, point * radian) 
+                                            for point in 1:num_of_points]
+    return points
+end
+
+
+"""
+generate a point at a distance n from the center
+"""
+function find_point_at_distance(radius::Int,centre::LLA, radian::Float64)
+    return LLA(ENU(radius*cos(radian),radius*sin(radian),0),centre)
+    
 end
 
 """
@@ -48,16 +52,18 @@ end
 - 'attribute'::Symbol - The category that will be used to calculate attractiveness
                         (:education, :entertainment, :healthcare, :leisure, :parking,
                         :restaurants, :shopping, :transport)
+if generate_sectors function used, there is no need to use the function - attractiveness of
+each point is already generated
 """
-
-
 
 function calculate_attractiveness_of_sector(points_matrix,attractivenessSpatIndex,
                                                                 attribute::Symbol)
+
     dim1, dim2 = size(points_matrix)
     attract = Array{Float64}(undef, dim1)
+    attr = zeros(Float64,dim2)
     for i in 1:dim1
-        attr = zeros(Float64,dim2)
+        fill!(attr, 0.0)
         for j in 1:dim2
             attr[j] = getfield(OSMToolset.attractiveness(
                 attractivenessSpatIndex,points_matrix[i,j]),attribute)
