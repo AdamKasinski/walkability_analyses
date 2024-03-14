0dfc60b9b54702acaf824c8716145b22b238a276
Adam Kasinski <adam.kasinski9@gmail.com>
2024-03-14 03:00:31 +0100

    plot function added

---
+++ plots.jl
@@ -0,0 +1,30 @@
+using Luxor
+using PythonCall
+using Colors
+using OpenStreetMapXPlot
+using Plots
+
+folium = pyimport("folium")
+cm = pyimport("branca.colormap")
+
+function plot_heatmap(city_centre, city_points, attractiveness_points,
+                        zoom_start = 14, control_scale = false)
+    
+    min_attr = minimum(attractiveness_points)
+    max_attr = maximum(attractiveness_points)
+
+    linear = cm.LinearColormap(["red", "yellow", "green"], 
+                                            vmin=min_attr, 
+                                            vmax=max_attr)
+
+    for sector in 1:size(points_cracow)[1]
+        for point in 1:size(points_cracow)[2]
+            lt = points_cracow[sector,point].lat
+            ln = points_cracow[sector,point].lon
+            flm.Circle([lt,ln],popup=sector, radius =5,
+                        color=linear(cracow_attr_matrix[sector,point])).add_to(map_plot)
+        end
+    end
+
+
+end
